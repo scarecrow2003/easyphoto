ddfbef11e748086528553d42bcd331e67656d2f6
zhihua.su@goldenequator.com <zhihua.su@goldenequator.com>
2016-10-14 18:57:15 +0800

    browserify

--- frontend/easyphoto/app.jsx
+++ frontend/easyphoto/app.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, IndexRoute, Route, browserHistory} from 'react-router';
-import Home from 'app/component/Home.jsx';
+import RouterContainer from 'app/RouterContainer';
+import Home from 'app/components/Home.jsx';
 
 const routeConfig = [
   {path: 'app', component: Home}
-]
+];
 
-ReactDOM.render(
-    <Router history={browserHistory} routes={routeConfig} />, document.getElementById('content')
-);
+ReactDOM.render(<Router history={browserHistory} routes={routeConfig} />, document.getElementById('content'));
 
