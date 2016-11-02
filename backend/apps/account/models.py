15881ff5a2bd244b3ccf541c4c694f45c762e7b6
zhihua.su@goldenequator.com <zhihua.su@goldenequator.com>
2016-11-02 13:00:34 +0800

    company

--- backend/apps/account/models.py
+++ backend/apps/account/models.py
@@ -1,13 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 
 class EPAccount(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nick_name = models.CharField(max_length=30)
+    wechat_id = models.CharField(max_length=50, blank=True)
 
     class Meta:
         app_label = 'account'
+        db_table = 'account'
 
     def __str__(self):
         return self.nick_name
+
+
+class EPCompany(models.Model):
+    group = models.OneToOneField(Group, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=50)
+
+    class Meta:
+        app_label = 'account'
+        db_table = 'company'
+
+    def __str__(self):
+        return self.company_name
--- backend/apps/account/views.py
+++ backend/apps/account/views.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.http import JsonResponse, HttpResponseRedirect
 from django.contrib.auth import login as lg, authenticate
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework_jwt.settings import api_settings
 import json
-from .models import EPAccount
+from .models import EPAccount, EPCompany
 
 @csrf_exempt
 def signup(request):
@@ -13,15 +13,26 @@ def signup(request):
     email = data.get('email')
     password = data.get('confirmed_password')
     nick_name = data.get('nick_name')
+    is_company = data.get('is_company')
+    company_name = data.get('company_name')
     try:
         user = User.objects.get(email=email)
         return JsonResponse({'error': 'The email address you entered has already been used'})
     except User.DoesNotExist:
+        if is_company:
+            try:
+                EPCompany.objects.get(company_name=company_name)
+                return JsonResponse({'error': 'The company name you entered has already been used'})
+            except EPCompany.DoesNotExist:
+                group = Group.objects.create(name=user_name)
+                group.save()
+                company = EPCompany.objects.create(group=group, company_name=company_name)
         user = User.objects.create_user(user_name, email, password, first_name=nick_name, last_name='')
         user.is_active = True
         user.save()
         user.backend = 'django.contrib.auth.backends.ModelBackend'
         account = EPAccount.objects.create(user=user, nick_name=nick_name)
+        user.groups.add(group)
         lg(request, user)
         return JsonResponse({'token': generate_token(user)})
 
